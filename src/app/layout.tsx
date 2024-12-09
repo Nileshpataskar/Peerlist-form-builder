ec4a3ae9cfdca5dc249f0ac6e0bd7fb12028e0d8
Nilesh Pataskar <nileshpataskars@gmail.com>
2024-12-09 13:59:34 +0530

    next font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 
-import { Poltawski_Nowy } from "@next/font/google";
+import { Poltawski_Nowy } from "next/font/google";
 import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
